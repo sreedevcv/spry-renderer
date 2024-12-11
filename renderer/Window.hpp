3e78b8f336c323b07faef1c908a09f2629a44583
Sreedev <sreedevcv0@gmail.com>
2024-12-11 22:00:45 +0530

    Changed some methods from public to protected in Window.cpp

--- renderer/Window.hpp
+++ renderer/Window.hpp
@@ -10,20 +10,21 @@ public:
     Window(int width, int height, const char* title, bool debug_mode = false);
     virtual ~Window();
 
+    void start();
+    void closeWindow() const;
+
     bool isKeyPressed(int key) const;
     bool isKeyReleased(int key) const;
     double getGlobalTime() const;
+
+protected:
     void setMouseCapture(bool capture) const;
     void setWireFrameMode(bool value) const;
     void setCulling(bool value) const;
     void setDepthTest(bool value) const;
     void setBlending(bool value) const;
-
-    void start();
-    void closeWindow() const;
     GLFWwindow* get_window() const;
 
-protected:
     virtual void onUpdate(float deltaTime) = 0;
     virtual void onMouseMove(double xPosIn, double yPosIn);
     virtual void onMouseScroll(double xOffset, double yOffset);
